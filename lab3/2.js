308a37a8a1f8885fde5b9d59fe687b02a0b2f7f5
Sanyazay <ScaredSnael3@mail.ru>
2022-03-11 00:09:28 +0300

    lab3

--- lab3/2.js
+++ lab3/2.js
@@ -7,8 +7,23 @@
  * Примеры:
  * '4 и -6, 2, 1, может 9, 63, -134 и 566]' -> {min: -134, max: 566}
  */
-function getMinMax(str) {
-    // code here
+ function getMinMax(str) {
+    let elements = str.replace(/[^0-9 -.]/g,"").replace(/\s{2,}/g," ").replace(/[-]{2,}/g,"-").split(" ");
+    elements = elements.filter(x => x!="");
+    let min = Number(elements[0]);
+    let max = Number(elements[0]);
+    for(let i = 0; i < elements.length; i++){
+        if(min > Number(elements[i])){
+            min = Number(elements[i])
+        }
+        if(max < Number(elements[i])){
+            max = Number(elements[i])
+        }
+    }
+    let result = {};
+    result["min"] = min;
+    result["max"] = max;
+    return result
 }
 
 module.exports = getMinMax;
